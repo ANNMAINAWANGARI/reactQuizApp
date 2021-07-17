99815239310e1370432f3c8c71f15403c301248d
ANNMAINAWANGARI <50842222+ANNMAINAWANGARI@users.noreply.github.com>
2021-07-17 22:12:14 +0300

    add dummy questions data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,68 @@ import Trivia from './components/Trivia'
 import './App.css';
 function App() {
   const [questionNum,setQuestionNum]=useState(5);
+  const data =[
+    {
+      id:1,
+      question:'Who is the first president of Kenya?',
+      answers:[
+        {text:'Uhuru Kenyatta',
+         correct:false
+        },
+        {text:'Jaramogi Oginga',
+         correct:false},
+        {text:'Jomo Kenyatta',
+        correct:true},
+        {text:'Raila Odinga',
+        correct:false}
+      ]
+    },
+    {
+      id:2,
+      question:'What color is the sky?',
+      answers:[
+        {text:'green',
+         correct:false
+        },
+        {text:'white',
+         correct:false},
+        {text:'blue',
+        correct:true},
+        {text:'yellow',
+        correct:false}
+      ]
+    },
+    {
+      id:3,
+      question:'What are greenhouses made of?',
+      answers:[
+        {text:'bricks',
+         correct:false
+        },
+        {text:'cotton',
+         correct:false},
+        {text:'paper',
+        correct:true},
+        {text:'wood',
+        correct:false}
+      ]
+    },
+    {
+      id:4,
+      question:'Which is the best programming pc?',
+      answers:[
+        {text:'Mac',
+         correct:false
+        },
+        {text:'Windows',
+         correct:false},
+        {text:'Linux',
+        correct:true},
+        {text:'Ubuntu',
+        correct:false}
+      ]
+    }
+  ]
   const cash =[
     {id:1,amount:'$ 1000000'},
     {id:2,amount:'$ 2000000'},
