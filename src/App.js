f9048a93a23d6f2f0164631dc1d1c21a27292e5c
ANNMAINAWANGARI <50842222+ANNMAINAWANGARI@users.noreply.github.com>
2021-07-27 13:17:23 +0300

    restructure app.js code

--- src/App.js
+++ src/App.js
@@ -1,117 +1,36 @@
-import React,{useState,useEffect} from 'react'
-import Trivia from './components/Trivia'
+import React,{useState} from 'react'
+import Header from './components/Header'
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Results from './pages/Results'
+import Trivia from './pages/Trivia'
 function App() {
-  const [questionNum,setQuestionNum]=useState(1);
-  const [stop,setStop]=useState(false);
-  const [earned,setEarned]=useState('$ 0')
-  const data =[
-    {
-      id:1,
-      question:'Who is the first president of Kenya?',
-      answers:[
-        {text:'Uhuru Kenyatta',
-         correct:false
-        },
-        {text:'Jaramogi Oginga',
-         correct:false},
-        {text:'Jomo Kenyatta',
-        correct:true},
-        {text:'Raila Odinga',
-        correct:false}
-      ]
-    },
-    {
-      id:2,
-      question:'What color is the sky?',
-      answers:[
-        {text:'green',
-         correct:false
-        },
-        {text:'white',
-         correct:false},
-        {text:'blue',
-        correct:true},
-        {text:'yellow',
-        correct:false}
-      ]
-    },
-    {
-      id:3,
-      question:'What are greenhouses made of?',
-      answers:[
-        {text:'bricks',
-         correct:false
-        },
-        {text:'cotton',
-         correct:false},
-        {text:'paper',
-        correct:true},
-        {text:'wood',
-        correct:false}
-      ]
-    },
-    {
-      id:4,
-      question:'Which is the best programming pc?',
-      answers:[
-        {text:'Mac',
-         correct:false
-        },
-        {text:'Windows',
-         correct:false},
-        {text:'Linux',
-        correct:true},
-        {text:'Ubuntu',
-        correct:false}
-      ]
-    }
-  ]
-  const cash =[
-    {id:1,amount:'$ 1000000'},
-    {id:2,amount:'$ 2000000'},
-    {id:3,amount:'$ 3000000'},
-    {id:4,amount:'$ 4000000'},
-    {id:5,amount:'$ 5000000'},
-    {id:6,amount:'$ 6000000'},
-    {id:7,amount:'$ 7000000'},
-    {id:8,amount:'$ 800000000'},
-    {id:9,amount:'$ 90000000000'},
-    {id:10,amount:'$ 1000000000000'}
-    ,
-  ].reverse();
-  useEffect(()=>{
-  questionNum >1 &&
-  setEarned(
-    cash.find((m)=>m.id ===questionNum-1).amount
+  const [name,setName] = useState();
+  const fetchQuestions = ()=>{
+
+  }
+  return(
+    <Router>
+     <div className='app'>
+      <Header/> 
+      <Switch>
+        <Route path='/' exact>
+          <Home name={name}setName={setName}fetchQuestions={fetchQuestions}/>
+        </Route>
+        <Route path='/results'>
+          <Results/>
+        </Route>
+        <Route path='/trivia'>
+          <Trivia/>
+        </Route>
+      </Switch>
+     </div>
+   </Router>
   )
-  },[cash,questionNum])
-  return (
-    <div className="app">
-     <div className='app_left'>
-       {stop? <h1 className='endText'>You earned {earned}</h1> :(
-      <>
-       <div className='appLeft_top'>
-         <div className='app_timer'>30</div>
-       </div>
-       <div className='appLeft_bottom'>
-         <Trivia data={data}setStop={setStop}setQuestionNum={setQuestionNum}questionNum={questionNum}/>
-       </div>
-       </>)}
-     </div>
-     <div className='app_right'>
-       <ul className='moneyList'>
-         {cash.map((c)=>(
-           <li className={questionNum === c.id ? 'moneyList_item active':'moneyList_item'}>
-           <span className='moneyList_number'>{c.id}</span>
-           <span className='moneyList_amount'>{c.amount}</span>
-         </li>
-         ))}
-         
-       </ul>
-     </div>
-    </div>
-  );
+  
+  
+ 
 }
 
 export default App;
