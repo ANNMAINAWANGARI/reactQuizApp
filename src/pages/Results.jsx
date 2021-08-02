1f3e6f135a6363e7727b367247c34b17acf40ffe
ANNMAINAWANGARI <50842222+ANNMAINAWANGARI@users.noreply.github.com>
2021-08-02 12:22:52 +0300

    results page logic

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,9 +1,31 @@
+import { Button } from '@material-ui/core';
 import React from 'react'
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "./Results.css";
 
-function Results() {
+function Results({score,name}) {
+
+ const history = useHistory();
+
+  useEffect(() => {
+    if (!name) {
+      history.push("/");
+    }
+  }, [name, history]);
+
  return (
-  <div>
-   
+  <div className='results'>
+   <h3>FINAL SCORE:{score}</h3>
+   <Button 
+        variant="contained"
+        color="secondary"
+        size="large"
+        style={{ alignSelf: "center", marginTop: 20 }}
+        href="/"
+      >
+        Go to homepage
+      </Button>
   </div>
  )
 }
